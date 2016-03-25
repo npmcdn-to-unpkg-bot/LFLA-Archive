b088ea160939acfefe98b94e11cc16081db925ce
Craig Fowler <craig@wearegiants.com>
2016-03-25 16:22:19 -0700

    updated aloud colors

--- lib/media_archive.php
+++ lib/media_archive.php
@@ -5,12 +5,34 @@
 function media_category() {
 
   $taxonomy = 'tribe_events_cat';
-  if(is_single()){
-    $terms = get_the_term_list( $post->ID, 'tribe_events_cat', '<ul class="archive__cats"><li><a href="/">Media Archive</a>', ',</li><li>', '</li></ul>' );
-  } else {
-    $terms = get_the_term_list( $post->ID, 'tribe_events_cat', '<ul class="archive__cats"><li>', ',</li><li>', '</li></ul>' );
-  }
-  return $terms;
+  //$terms = get_the_term_list( $post->ID, 'tribe_events_cat', '<ul class="archive__cats featured"><li class="'. $term->name .'"><span class="bg--blue">Featured in</span> ', ',</li><li>', '//</li></ul>' );
+  //return $terms;
+  $terms = get_the_terms( $post->ID , $taxonomy );
+  //var_dump($terms);
+
+  echo '<ul class="archive__cats">';
+
+  foreach($terms as $term):
+
+    $termName = $term->name;
+    
+    if( $term->slug == 'aloud' ){ 
+      $color = 'pink';
+    } else {
+      $color = 'blue';
+    };
+
+    echo '<li class=" ' . $term->slug . ' bg--' . $color . '">'; 
+    if(is_single()){
+      echo '<a href="/">Media Archive</a>';
+    }
+    echo '<span>'; 
+    echo $termName;
+    echo '</span>'; 
+    echo '</li>'; 
+  endforeach;
+
+  echo '</ul>';
 
 }
 
